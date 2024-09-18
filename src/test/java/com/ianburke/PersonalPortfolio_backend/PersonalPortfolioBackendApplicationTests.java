25689c79ff37408e70abaa2d442e2918bf1ffad5
IanBurke1 <ianburke147@gmail.com>
2024-09-18 19:29:27 +0100

    Initial commit of personal portfolio spring boot backend

---
+++ src/test/java/com/ianburke/PersonalPortfolio_backend/PersonalPortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianburke.PersonalPortfolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
